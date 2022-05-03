a98f61b73cd45dd719bedbcd4cdae82f0d7f6b91
jona <jevkjev@gmail.com>
2022-05-03 12:15:59 -0300

    mejorando un poco mas

--- demo-proyecto-angular/src/app/components/punto2/punto2.component.ts
+++ demo-proyecto-angular/src/app/components/punto2/punto2.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./punto2.component.css']
 })
 export class Punto2Component implements OnInit {
-  palabras = ['MANZANA', 'PERA', 'MANDARINA','ANANA','POMELO','LIMON','NARANJA','BANANA','HIGO','UBA'];
+  palabras = ['MANZANA', 'PERA', 'MANDARINA','ANANA','POMELO','LIMON','NARANJA','BANANA','HIGO','UVA'];
   indice = Math.floor((Math.random() * (10)));
   palabra = this.palabras[this.indice];
   palabraOculta= '';
